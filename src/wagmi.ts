31aca051e0c2c381a5d95d250932260cac351c97
dadayunghub <davidyung450@gmail.com>
2024-09-09 17:56:10 +0000

    Initial commit

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,83 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { createConfig } from 'wagmi';
+import { http } from 'wagmi';
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import {
+bybitWallet,
+coinbaseWallet,
+  metaMaskWallet,
+  trustWallet,
+  coreWallet,
+  krakenWallet,
+  ledgerWallet,
+  okxWallet,
+  mewWallet,
+  phantomWallet,
+  walletConnectWallet,
+  bitgetWallet,
+  binanceWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import { 
+  mainnet, 
+  sepolia, 
+  polygon, 
+  optimism, 
   arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
+  base, 
+  zkSync,
+  bsc,
 } from 'wagmi/chains';
 
-export const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [walletConnectWallet, metaMaskWallet, binanceWallet, trustWallet],
+    },
+    
+    {
+      groupName: 'Suggested',
+      wallets: [
+        bybitWallet,
+coinbaseWallet,
+  
+  
+  coreWallet,
+  krakenWallet,
+  ledgerWallet,
+  okxWallet,
+  mewWallet,
+  phantomWallet,
+  
+  bitgetWallet,
+  
+      ],
+    },
+  ],
+  { appName: 'Chron Node', projectId: '7e02e091c30851f318144e3327aa8c63' },
+);
+
+export const config = createConfig({
   chains: [
     mainnet,
+    sepolia,
     polygon,
     optimism,
     arbitrum,
     base,
+    zkSync,
+    
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
+  connectors: connectors,
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [polygon.id]: http(),
+    [optimism.id]: http(),
+    [arbitrum.id]: http(),
+    [base.id]: http(),
+    [zkSync.id]: http(),
+    [bsc.id]: http(),
+  },
   ssr: true,
 });
